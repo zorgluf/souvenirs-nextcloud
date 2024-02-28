4adec84a53b6a8edf0e552bf55cd60ea54af72be
zorgluf <zorgluf@gmail.com>
2024-02-28 22:01:41 +0100

    fix zoom on videos

--- vite-src/components/selement.vue
+++ vite-src/components/selement.vue
@@ -4,7 +4,7 @@
     v-bind:style="'top:'+sTop.toString()+'%;left:'+sLeft.toString()+'%;width:'+(sRight-sLeft).toString()+'%;height:'+(sBottom-sTop).toString()+'%;--image-src-url:url(\''+sImageSrc+'\')'">
 		<div class="s-element-text resize" v-if="(sText)">{{sText}}</div>
         <video v-bind:id="sId+'video'" v-if="sClass.endsWith('VideoElement')" v-on:click="openVideo"
-            v-bind:class="['video-element', isImgCenterCrop ? 'centercrop' : '', isImgFill ? 'fill' : '' ]" 
+            v-bind:class="['video-element', sZoom == 100 ? 'fill' : 'centercrop' ]" 
             loop="true" preload="auto" :key="videoUrlSrc"
             v-on:waiting="waitingVideo" v-on:canplay="playingVideo" v-on:loadstart="waitingVideo">
             <source v-bind:src="videoUrlSrc">
@@ -261,7 +261,6 @@ center {
 }
 
 .centercrop {
-    object-fit: cover;
     width: 100%;
 	height: 100%;
 }
